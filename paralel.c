3df2a833ce4e9b6ff6568afe5bc9435d1dab10ac
Kado.cbc <kado.cbc@gmail.com>
2025-10-10 11:47:06 -0300

    add: codigo produtor/consumidor

--- paralel.c
+++ paralel.c
@@ -4,67 +4,109 @@
 #include <semaphore.h>
 #include "paralel.h"
 
+//LOGICA ATÉ O MOMENTO -> CRIAÇÃO DO CODIGO PRODUTOR CONSUMIDOR
+// E LOGICA DE POOL DE THREADS, FALTA AINDA ENTENDER COMO TRASNFORMAR ISSO
+// EM UMA BIBLIOTECA QUE TEM UMA FUNÇÃO PARA ENVIAR AS TAREFAS 
 
-int pollt = 4;
-pthread_t threads[pollt];
-int threads_id[pollt];
+
+//Tamanho da pool de threads
+#define T_POOL 5
+//lista de threads
+pthread_t threads[T_POOL];
+int threads_id[T_POOL];
+
+// Lista = buffer do padrao produtor consumidor 
+int fila[T_POOL];
+int contador_fila = 0;
 
 
-sem_t employer;
-sem_t master;
+// inicialização semafaro para padrão produtor consumidor (Como só tem um produtor precisa fazer um semafaro para produzir ?)
+sem_t espaco;
+sem_t ocupado;
+//incializacao do mutex para add no buffer
 pthread_mutex_t mutex;
 
-//protoripo de uma fila de tasks com tamanho igual numero de threads
-task fila[pollt];
-int contador_fila;
-
-//função para tirar da fila (deve ser um struct depois provavelmente)
-void dequeue(int n_task){
-    pthread_mutex_lock(&mutex);
-    fila[n] = NULL;  //duvida de como usar o mutex nesse caso
-    --contador_fila;
-    pthread_mutex_unlock(&mutex);
-    
-};
-//função para colocar na fila (deve ser um struct depois provavelmente)
-void queue(task *n){
-    if (contador_fila >= 4){
-        wait(&master); 
-    };
-    pthread_mutex_lock(&mutex);
-    fila[contador_fila + 1 ] = n;
-    ++contador_fila
-    pthread_mutex_unlock(&mutex);
-};
-
 //Thread que irá fazer a chamada das atividades para pool -> produtora
-void *thread_master(void *args){
+void *thread_mestre(void *args){
 
+    //talvez tenha um While aqui()
+    int task = 0;//temporario irá receber dos args
+    
+    // espera por um sinal de espaço 
+    sem_wait(&espaco);
+
+    // bloqueia o mutex para buffer (seção critica)
+    pthread_mutex_lock(&mutex);
+
+    //coloca task na fil de tarefas (buffer)
+    fila[contador_fila] = task;
+    printf("tarefa adicionada: %d\n", task)
+    contador_fila = contador_fila + 1;
+
+    // libera mutex
+    pthread_mutex_unlock(&mutex);
+
+    // manda sinal de nova task
+    sem_post(&ocupado);
 
 };
 
 //Threads que irá exucutar a lista de atividas -> consumidora
-void *thread_employers(void *args){
+void *thread_workers(void *args){
+    
+    //espera sinal de nova taks
+    sem_wait(&ocupado);
 
+    //bloqueia o mutex 
+    pthread_mutux_lock(&mutex);
+
+    //tira task na fil de tarefas (buffer)
+    int task = fila[contador_fila];
+    //executa a task (fazer codigo)
+    printf("tarefa executada: %d\n", task)
+    contador_fila = contador_fila - 1;
+
+    // libera mutex
+    pthread_mutex_unlock(&mutex);
+
+    // sinaliza novo espaco no buffer
+    sem_post(&espaco);
 };
 
 
 //Logica para criar uma pool de threads baseando em produtor/consumidor. 
-int createPool(pollt){
+int createPool(){
 
     int i = 0;
 
-    pthread_create(&threds[i], NULL, thread_master, &threads_id[i]);
+    pthread_create(&threds[i], NULL, thread_mestre, &threads_id[i]);
     
     
-    for (i = 1, i < pollt, i++){
+    for (i = 1, i < T_POOL, i++){
         threads_id[i] = i;
-        pthread_create(&threds[i], NULL, thread_employers, &threads_id[i]);
-    }; 
+        pthread_create(&threds[i], NULL, thread_workers, &threads_id[i]);
+    };
+
+    for(i = 1; i < T_POOL; i++){
+        pthread_join(threads[i],NULL);
+    };
+
+    pthread_cancel(threads[0]); // thread mestre
 };
 
+//codigo incompleto, falta entender como realizar o escopo
+int execute() {
 
-void execute(){
+    sem_init(&ocupado, 0, 0); // inicia semafaro "fechado"
+    sem_init(&espaco, 0, T_POOL); //inicia semafaro a partir do tamanho da pool
+    pthread_mutex_init(&mutex, NULL);
 
-    return
+    //cria a pool
+    createPool();
+
+    //destroi os semafaros
+    sem_destroy(&ocupado);
+    sem_destroy(&espaco);
+    pthread_mutex_destroy(&mutex);
+
 };
